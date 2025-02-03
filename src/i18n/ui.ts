769ec5f2b47548efe4b792335c6b3424d80dfb03
Zhuo <nicodiazan@gmail.com>
2025-02-03 20:56:29 +0100

    Add the basics for language switching

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+
+export const languages = {
+    en: "English",
+    es: "Español",
+}
+
+export const defaultLang = "es";
+
+export const ui = {
+	en: {
+		"nav.home": "Home",
+		"nav.about": "About",
+		"nav.contact": "Contact",
+		"nav.projects": "Projects",
+		"nav.experience": "Experience",
+	},
+	es: {
+		"nav.home": "Inicio",
+		"nav.about": "Sobre mí",
+		"nav.contact": "Contacto",
+		"nav.projects": "Proyectos",
+		"nav.experience": "Experiencia",
+	},
+} as const;
