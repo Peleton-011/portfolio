7cdab2cba6413af19698f5b72bd895bbb8408de4
Zhuo <nicodiazan@gmail.com>
2025-02-12 17:33:26 +0100

    Add a tags file for neatness and organization

---
+++ src/i18n/tags.ts
@@ -0,0 +1,92 @@
+import AstroIcon from "../components/icons/AstroIcon.astro";
+import ClerkIcon from "../components/icons/ClerkIcon.astro";
+import CSSIcon from "../components/icons/CSSIcon.astro";
+import HTMLIcon from "../components/icons/HTMLIcon.astro";
+import JSIcon from "../components/icons/JSIcon.astro";
+import MongoIcon from "../components/icons/MongoIcon.astro";
+import NextIcon from "../components/icons/NextIcon.astro";
+import NodeIcon from "../components/icons/NodeIcon.astro";
+import NuxtIcon from "../components/icons/NuxtIcon.astro";
+import ReactIcon from "../components/icons/ReactIcon.astro";
+import TailwindIcon from "../components/icons/TailwindIcon.astro";
+import TSIcon from "../components/icons/TSIcon.astro";
+import ViteIcon from "../components/icons/ViteIcon.astro";
+import VueIcon from "../components/icons/VueIcon.astro";
+
+export const TAGS = {
+	ASTRO: {
+		name: "Astro",
+		class: "bg-[#FF5A00] text-white",
+		icon: AstroIcon,
+	},
+	CLERK: {
+		name: "Clerk",
+		class: "bg-black text-white",
+		icon: ClerkIcon,
+	},
+	CSS: {
+		name: "CSS",
+		class: "bg-[#1572B6] text-white",
+		icon: CSSIcon,
+	},
+    CSSMODULES: {
+        name: "CSS Modules",
+        class: "bg-[#1572B6] text-white",
+        icon: CSSIcon,
+    },
+	HTML: {
+		name: "HTML",
+		class: "bg-[#E34C26] text-white",
+		icon: HTMLIcon,
+	},
+	JS: {
+		name: "JavaScript",
+		class: "bg-[#F7DF1E] text-black",
+		icon: JSIcon,
+	},
+	MONGODB: {
+		name: "MongoDB",
+		class: "bg-[#47A248] text-white",
+		icon: MongoIcon,
+	},
+	NEXT: {
+		name: "Next.js",
+		class: "bg-black text-white",
+		icon: NextIcon,
+	},
+	NODE: {
+		name: "Node.js",
+		class: "bg-[#339933] text-white",
+		icon: NodeIcon,
+	},
+	NUXT: {
+		name: "Nuxt.js",
+		class: "bg-[#00DC82] text-black",
+		icon: NuxtIcon,
+	},
+	REACT: {
+		name: "React",
+		class: "bg-[#61DAFB] text-black",
+		icon: ReactIcon,
+	},
+	TAILWIND: {
+		name: "Tailwind CSS",
+		class: "bg-[#003159] text-white",
+		icon: TailwindIcon,
+	},
+	TS: {
+		name: "TypeScript",
+		class: "bg-[#007ACC] text-white",
+		icon: TSIcon,
+	},
+	VITE: {
+		name: "Vite",
+		class: "bg-[#41B3A3] text-white",
+		icon: ViteIcon,
+	},
+	VUE: {
+		name: "Vue",
+		class: "bg-[#41B883] text-white",
+		icon: VueIcon,
+	},
+};
